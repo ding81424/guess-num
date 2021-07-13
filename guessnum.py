4b734ca69f5b23475771546e725dd8e78b96582f
Danny Ding <ding81424@gmail.com>
2021-07-13 11:02:27 +0800

    User can decide range

--- guessnum.py
+++ guessnum.py
@@ -1,12 +1,18 @@
 import random
 
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍起始值： ')
+end = input('請決定隨機數字範圍結束值： ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1 
 	num = input('請猜數字？ ')
 	num = int(num)
-	if num == r:
+	if num < start or num > end:
+		print('此數字不在範圍內！')
+	elif num == r:
 		print('你猜對了！')
 		print('這是你猜的第', count, '次！')
 		break
